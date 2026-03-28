85a0c3427956d357bc2425bef6eaba40ebfe6085
Aminul islam <aminurrohman98@gmail.com>
2026-03-28 20:16:46 +0000

    Implement task addition functionality in AddTaskSection component

--- src/AddTaskSection/AddTaskSection.jsx
+++ src/AddTaskSection/AddTaskSection.jsx
@@ -1,20 +1,51 @@
-const AddTaskSection = () => {
+import { useState } from "react";
+
+const AddTaskSection = ({ todoList, setTodoList }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [priority, setPriority] = useState("low");
+  const addTaskHandler = () => {
+    setTodoList(() => {
+      return [
+        ...todoList,
+        {
+          id: Math.random(),
+          task: inputValue,
+          priority,
+          completed: false,
+        },
+      ];
+    });
+  };
+
   return (
     <div className="add-task-section rounded-2xl p-4 max-w-5xl w-full mx-auto border border-gray-300 mt-10">
       <div className="add-task-section-content w-full border border-gray-300 rounded-lg space-y-2">
         <h2 className="font-semibold text-md">What do you need to add ?</h2>
         <input
+          // setting up the input value and onChange handler to update the state
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           className="w-full p-1 border border-gray-300 text-lg text-gray-800 font-semibold rounded "
           placeholder="Type your task here..."
           type="text"
         />
         <div className="flex flex-row md:gap-4 gap-2">
-          <select className="priority-section border border-gray-400 text-black font-bold py-2 px-4 rounded-xl w-1/3">
+          <select
+            className="priority-section border border-gray-400 text-black font-bold py-2 px-4 rounded-xl w-1/3"
+            onChange={(e) => {
+              setPriority(e.target.value);
+            }}
+          >
             <option value="low">Low</option>
             <option value="medium">Medium</option>
             <option value="high">High</option>
           </select>
-          <button className="border border-gray-400 text-black font-bold py-2 px-4 rounded-xl w-full cursor-pointer hover:bg-gray-200 transition duration-300 ">
+          <button
+            onClick={() => {
+              addTaskHandler();
+            }}
+            className="border border-gray-400 text-black font-bold py-2 px-4 rounded-xl w-full cursor-pointer hover:bg-gray-200 transition duration-300 "
+          >
             + Add Task
           </button>
         </div>
--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import AddTaskSection from "./AddTaskSection/AddTaskSection.jsx";
 import "./App.css";
 import Navbar from "./navbar/Navbar.jsx";
 import TodoCard from "./todoTask/TodoCard.jsx";
+import { useState } from "react";
 
 function App() {
-  // const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([]);
+  console.log(todoList);
 
   return (
     <div>
       <Navbar></Navbar>
       <AddTaskSection
-      // todoList={todoList}
-      // setTodoList={setTodoList}
+        todoList={todoList}
+        setTodoList={setTodoList}
       ></AddTaskSection>
       <TodoCard></TodoCard>
     </div>
